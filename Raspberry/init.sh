0326abb423f7ca1b72f42423d9f4328e7b43afcc
qfdk <qfdk@vip.qq.com>
2016-04-21 15:45:46 +0200

    = =

--- Raspberry/init.sh
+++ Raspberry/init.sh
@@ -2,7 +2,7 @@
 
 ## installer gstreamer 
 sudo echo "deb http://vontaene.de/raspbian-updates/ . main" >>/etc/apt/sources.list
-sudo apt-get -y gstreamer1.0
-
+sudo apt-get update
+sudo apt-get install gstreamer1.0
 ## rediriger le flux
 raspivid -n -t 0 -rot 270 -w 960 -h 720 -fps 30 -b 6000000 -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=pi.qfdk.me port=5000
