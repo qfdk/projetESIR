5f496309d6d006b32da7dc74b2ce8afab5dc5bd6
ilyass <hp@hp-PC>
2016-05-03 16:59:27 +0200

    ajout de nouveau fichiers, et je n'aime pas les vielles je rigolais

--- Web/assets/js/app.js
+++ Web/assets/js/app.js
@@ -1,6 +1,30 @@
 
 
+/*
+$('#btnpressed').click(function(e) {
+  //ajax	 		
+   //e.preventDefault();
+	$.post('/createaccount/newuser',{
 
-$('#btnCreate').click(function() {
-  //ajax
+	})
 });
+*/
+	var socket = io.connect('http://localhost:3000');
+
+$('#loginform').submit(function(){
+	
+		socket.emit('createaccount',{
+			firstname :$('#nom').val(),
+			secondname :$('#prenom').val(),
+			email :$('#email').val(),
+			mdp :$('#mdp').val(),
+			identifiant:$('#identifiant').val()
+		})
+	});
+
+$('#connectionform').submit(function(){
+		socket.emit('login',{
+			email :$('#email').val(),
+			password :$('#password').val()
+		})
+	});
--- Web/server.js
+++ Web/server.js
@@ -1,6 +1,7 @@
 
 // ---------------Environnement-----------------
 var mysql = require('node-mysql');
+var DB = mysql.DB;
 var url = require('url');
 var app = require('express')();
 var server = require('http').Server(app);
@@ -13,12 +14,27 @@ app.set('view engine', 'ejs');
 app.use(require('express').static(path.join(__dirname, './assets')));
 server.listen(3000);
 
+var io=require('socket.io').listen(server);
+
+
+var db =  new DB({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'Projet_esir',
+    connectionLimit: 50,
+    useTransaction: {
+        connectionLimit: 1
+    }
+});
 // -----------------index-----------------
 app.get('/index', function(req, res) {
     res.render('index');
 });
 
-
+app.get('/', function(req, res) {
+ res.render('index');
+});
 app.get('/login', function(req, res) {
 res.render('login');
 });
@@ -33,23 +49,61 @@ res.render('createaccount');
 });
 
 
-// function BD() {
-//     var connection = mysql.createConnection({
-//         user: 'root',
-//         password: 'coucousalifou',
-//         host: 'kfc-raspi',
-//        port: ,
-//         database: 'Projet_esir'
-//     });
-//     return connection;
-// }
+
+	 // L'utilisateur va créer un compte
+io.sockets.on('connection', function(socket){
+
+	socket.on('createaccount',function(user){
+	console.log("je suis la ");
+	var basicTest = function(cb) {
+    db.connect(function(conn, cb) {
+        cps.seq([
+            function(_, cb) {
+                conn.query('select * from  login_web', cb);
+
+            },
+            function(res, cb) {
+                console.log(res);
+                cb();
+            }
+        ], cb);
+    }, cb);
+};
+ });
+
+	// 
+	// 
+
+	// var post = {
+	// 	nom:user.firstname,
+	// 	prenom:user.secondname,
+	// 	identifiant:user.identifiant,
+	// 	mdp:user.mdp,
+	// 	email:user.email
+	// 	};
+ // 		db.query('INSERT INTO login_web VALUES ?', post, function(error) {
+ //        if (error) {
+ //            console.log(error.message);
+ //        } else {
+ //            console.log('success');    
+ //        }
+ //    });
+
+	// });
+
+socket.on('login',function(user){
+	console.log("je suis dans login ");
+});
+
+});
+
 
 // app.get("/user/create", function(req, res) {
 //     console.log("je suis la ");
 //         var objBD = BD();
 
 //     var post = {
-//         nom: req.body.nom,
+//      nom: req.body.nom,
 // 		prenom: req.body.prenom,
 // 		identifiant:req.body.id,
 // 		mdp:req.body.mdp,
