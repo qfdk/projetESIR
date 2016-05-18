8ffeb396ce4d959213555852f58498b5701e53e9
ymaiga <you@example.com>
2016-05-18 17:27:57 +0200

    login ok, reste la session

--- Web/server.js
+++ Web/server.js
@@ -7,7 +7,7 @@ var app = require('express')();
 var bodyParser = require("body-parser");
 var server = require('http').Server(app);
 var path = require('path');
-
+var cookieSession = require('cookie-session')
 
 // ------------express-------------------
 app.set('views', path.join(__dirname, 'view/'));
@@ -25,7 +25,7 @@ var io = require('socket.io').listen(server);
 var db = new DB({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'maiga',
     database: 'Projet_esir',
     connectionLimit: 50,
     useTransaction: {
@@ -43,7 +43,7 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 app.get('/login', function (req, res) {
-	res.render('login');
+	res.render('login', { 'erreur': '' });
 });
 
 app.get('/stream', function (req, res) {
@@ -52,15 +52,14 @@ app.get('/stream', function (req, res) {
         { name: 'Video 1', link: 'https://www.youtube.com/embed/qEOpts63QWg', description: "Decription de la video 1 ......" },
         { name: 'Docker', link: 'https://www.youtube.com/embed/060fY1KxTc', description: "Decription de la video 2 ......" },
         { name: 'Real', link: 'rtmp://rtmp.infomaniak.ch/livecast/yveline1', description: "Decription de la video 3 ......" },
-		{ name: 'Zizou', link: 'https://www.youtube.com/embed/GE5a6Q2NTKU', description: "Decription de la video frnce vs spain ......" }
+		{ name: 'Zizou', link: '//www.youtube.com/watch?v=GE5a6Q2NTKU', description: "Decription de la video frnce vs spain ......" }
     ];
 
 	res.render('streams', { urlsDispo });
 });
 
 app.post("/streamer", function (req, res) {
-	console.log(req.body);
-	res.render('streamer', {'stream':req.body.stream});
+	res.render('streamer', { 'stream': req.body.stream });
 });
 
 app.get('/createaccount', function (req, res) {
@@ -69,6 +68,30 @@ app.get('/createaccount', function (req, res) {
 
 });
 
+
+app.post('/login', function (req, res) {
+	var user = req.body.email;
+	var pass = req.body.pwd;
+
+	db.connect(function (conn) {
+		console.log(conn);
+		var requeteSql = 'SELECT * FROM login_web Where email = ? and mdp = ?';
+		conn.query(requeteSql, [user, pass], function (err, data) {
+			
+
+			if (data[0]!=undefined) {
+				console.log("Bien connecté ....");
+				res.redirect('stream');
+			}
+			else {
+				console.log("Identifiants incorrects ....");
+				res.render('login', { 'erreur': "Email ou mot de passe incorrect" });
+			}
+		});
+	});
+
+
+});
 
 // L'utilisateur va créer un compte
 io.sockets.on('connection', function (socket) {
@@ -122,11 +145,12 @@ io.sockets.on('connection', function (socket) {
 
 	// });
 
-	socket.on('login', function (user) {
-		console.log("je suis dans login ");
-	});
+	// socket.on('login', function (user) {
+	// 	console.log("je suis dans login ");
+	// });
 
 });
+
 
 
 // app.get("/user/create", function(req, res) {
