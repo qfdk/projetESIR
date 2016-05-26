0b1ad29ffbddbf5d1f442fbd2550807b86947430
qfdk <qfdk@vip.qq.com>
2016-05-26 23:46:25 +0200

    cosf

--- Linux/SmartPush/app.js
+++ Linux/SmartPush/app.js
@@ -13,9 +13,9 @@ var app = express();
 
 //session
 app.use(session({
-    secret: 'coucousalifou',
-    saveUninitialized:true,
-    resave: true
+	secret: 'coucousalifou',
+	saveUninitialized:true,
+	resave: true
 }));
 
 // view engine setup
@@ -29,10 +29,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res,next) {
+	var responseSettings = {
+		"AccessControlAllowOrigin": req.headers.origin,
+		"AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+		"AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+		"AccessControlAllowCredentials": true
+	};
+	res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+	res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+	res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+	res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+	if ('OPTIONS' == req.method) {
+		res.send(200);
+	}
+	else {
+		next();
+	}
+
+
+});
+
 app.use('/', routes);
 
 var http = require('http');
 http.createServer(app).listen(conf.port, function () {
-    console.log("[info] Server started : listen on " + conf.port)
+	console.log("[info] Server started : listen on " + conf.port)
 });
 module.exports = app;
